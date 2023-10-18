a13fb995da9eafdbe9c4a32d6eafa6f84b399cbf
Nina <catriona.hynes@dci-student.org>
2023-10-18 15:12:38 +0200

    index

---
+++ Nina_day1.py
@@ -0,0 +1,36 @@
+print("Hello World")
+print("I feel my brain growing")
+print("changing lines\
+ works")
+ #Variables
+ # 1. variables cannot start with numbers
+ # variables must have only alphanumeric characters and underscores
+ # first_name = "Nina"
+amount = 4000.50 # float
+# do some calculations
+amount = 3000 # int
+amount = "3000" # string
+# Integers 
+amount = 3400
+# type()
+# int are whole numbers, no decimal points 
+# the type function tells us what type of object the variable is referencing
+# print functions helps us to display that information in the terminal
+print(type(amount))
+# Float
+# Floats contain Decimal Points and numbers
+balance = 5000.78
+#print(type(balance))
+print(amount + balance)
+# Boolean
+# Boolean contains True or False 
+is_verified = True
+# String
+# Strings contain texteral information
+# Strings are within ("inverted commas") ("inside brackets")
+greeting = "Hello everybody"
+name = 'Nina'
+sentence = '''Hello it is a good day'''
+print('''Hello again''')
+
+
